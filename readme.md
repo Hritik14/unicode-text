34eec21124508ad7343739fa46da400de296eb4f
Hritik Vijay <hritikxx8@gmail.com>
2020-05-05 22:56:06 +0530

    Initial commit

---
+++ readme.md
@@ -0,0 +1,24 @@
+# What ? 
+Convert a normal looking English(Latin) text to fancy Unicode text.
+
+# Why ?
+- If you wanna mess with people
+- Just screwing around
+- Evade text filter detection 
+- Psst.. Your spam emails
+
+# How ?
+```sh
+❯ python unicode.py
+Taylor Swift
+𝚻𝐚𝓎𝗹ﮩ𝕣 𝘚ѡⅰ𝗳𝓉
+
+❯ python unicode.py
+Taylor Swift
+𝑇ａ𝘆𝖑ჿ𝓻 𐐠𝒘іẝ𝕥
+```
+
+Every invocation gives different result. Try until you're satisfied.
+
+## License ? 
+Kidding me ?
---
+++ unicode.py
@@ -0,0 +1,28 @@
+#!/bin/python
+import random
+
+CONFUSABLES = "./unicode.txt"
+string = input()
+m = {}
+with open(CONFUSABLES, "r") as f:
+    txt = f.read().split("\n")
+    for line in txt:
+        line = line.strip()
+        if len(line) == 0:
+            continue
+        if line[0] == '#':
+            continue
+        tokens = line.split(";")
+        if len(tokens) < 2:
+            continue
+
+        key = "".join([chr(int(t, 16)) for t in tokens[1].split()])
+        val = chr(int(tokens[0], 16))
+
+        if key in m:
+            m[key].append(val)
+        else:
+            m[key] = []
+
+for c in string:
+    print(random.choice(m[c]), end="")
